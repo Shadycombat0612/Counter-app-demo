1f577eb03c99e3843e92e7cae91431dd6eb8d6b6
HANUMANT SINGH NEGI <140183628+Shadycombat0612@users.noreply.github.com>
2023-08-26 12:42:39 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+// import logo from './logo.svg';
+
+// import './App.css';
+
+import { CounterPage } from "./pages/CounterPage";
+
+
+function App() {
+
+  return (<CounterPage/>)
+
+}
+
+
+export default App;
